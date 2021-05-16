8e2be874751b29dc7c96671434da37432f081958
Oscar Villagrana <47239158+oscarvillagrana@users.noreply.github.com>
2021-05-15 21:10:34 -0700

    testing cocoapods init

---
+++ Example/Pods/Target Support Files/PodTests/PodTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_PodTests : NSObject
+@end
+@implementation PodsDummy_PodTests
+@end
---
+++ Example/Pods/Target Support Files/Pods-PodTests_Example/Pods-PodTests_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PodTests_Example : NSObject
+@end
+@implementation PodsDummy_Pods_PodTests_Example
+@end
