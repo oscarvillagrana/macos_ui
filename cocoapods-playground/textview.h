aa3c8d97ba7324c31a1537a2cabcdb88f274b58f
Oscar Villagrana <oscarvillagrana@users.noreply.github.com>
2021-05-16 09:13:06 -0700

    added container functions

---
+++ cocoapods-playground/textview.h
@@ -0,0 +1,16 @@
+//
+//  textview.h
+//  cocoapods-playground
+//
+//  Created by Os on 5/16/21.
+//
+
+#import <Cocoa/Cocoa.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface textview : NSViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ cocoapods-playground/textview.m
@@ -0,0 +1,21 @@
+//
+//  textview.m
+//  cocoapods-playground
+//
+//  Created by Os on 5/16/21.
+//
+
+#import "textview.h"
+
+@interface textview ()
+
+@end
+
+@implementation textview
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do view setup here.
+}
+
+@end
