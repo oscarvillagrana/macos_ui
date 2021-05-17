03678d1fbc3d5b428e71fad8d779d0c1148f41c6
Oscar Villagrana <oscarvillagrana@users.noreply.github.com>
2021-05-16 21:18:59 -0700

    restart

--- Pods/Target Support Files/Pods-cocoapods-playground-cocoapods-playgroundUITests/Pods-cocoapods-playground-cocoapods-playgroundUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_cocoapods_playground_cocoapods_playgroundUITests : NSObject
-@end
-@implementation PodsDummy_Pods_cocoapods_playground_cocoapods_playgroundUITests
-@end
--- Pods/Target Support Files/Pods-cocoapods-playgroundTests/Pods-cocoapods-playgroundTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_cocoapods_playgroundTests : NSObject
-@end
-@implementation PodsDummy_Pods_cocoapods_playgroundTests
-@end
